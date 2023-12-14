9eafca5e024f92c1fbab71248b2e8293cfa9fbed
Allen <allen23648858@gmail.com>
2023-12-15 01:11:30 +0800

    fix websocket backend vendor 未連線/多人連線 bug

--- backend/models/websocket.model.js
+++ backend/models/websocket.model.js
@@ -1,6 +1,5 @@
 import { WebSocketServer } from 'ws';
 import { parse as urlParse } from 'url';
-
 export default class wsServer {
     constructor(server) {
         this.server = new WebSocketServer({ server });
@@ -8,13 +7,20 @@ export default class wsServer {
         this.server.on("connection", (connection, request) => {
             const { id, identity } = urlParse(request.url, true).query;
             console.log(`${identity} ${id} connect websocket`);
-            if (identity === 'vendor')
-                this.vendorConnections[id] = connection;
+            if (identity === 'vendor') {
+                if (!(id in this.vendorConnections)) {
+                    this.vendorConnections[id] = [];
+                }
+                this.vendorConnections[id].push(connection);
+            }
             connection.on('message', bytesmsg => {
                 const message = JSON.parse(bytesmsg.toString());
                 console.log(`ws recieve ${message}`);
-                this.vendorConnections[message.Vendor_ID].send(JSON.stringify(message));
-                
+                if (message.Vendor_ID in this.vendorConnections) {
+                    this.vendorConnections[message.Vendor_ID].forEach(vendorConnection => {
+                        vendorConnection.send(JSON.stringify(message));
+                    });
+                }
             });
         })
     }
