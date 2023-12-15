c04f15a3a970c3f58ca013084f4c52f0bb6ee846
Allen <allen23648858@gmail.com>
2023-12-15 20:37:50 +0800

    add DB connection pool

--- backend/models/dbasync.model.js
+++ backend/models/dbasync.model.js
@@ -2,27 +2,26 @@ import mysql from 'mysql2/promise.js';
 import { db_config } from "../config/db.config.js";
 
 console.log("Trying to connect to DB...");
-const connection = await mysql.createConnection({
+const pool = mysql.createPool({
     host: db_config.HOST,
     port: db_config.PORT,
     user: db_config.USER,
     password: db_config.PASSWORD,
-    database: db_config.DB
-});
-    
-// Connecting to database
-connection.connect(function (err) {
-    if (err) {
-        console.log("Error in the connection");
-        console.log(err);
-    }
-    else {
-        console.log(`Database Connected`);
-    }
+    database: db_config.DB,
+    waitForConnections: true,
+    connectionLimit: 50,
 });
 
+if (pool) {
+    console.log(`Database Connected`);
+}
+else {
+    console.log("Error in the connection");
+    console.log(err);
+}
+
 const query = async (sql, param) => {
-    return await connection.query(sql, param);
+    return await pool.query(sql, param);
 }
 
 export { query };
